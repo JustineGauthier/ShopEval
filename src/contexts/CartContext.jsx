0b8e8401c5bf6423d2a80bb499a64ae3084bab43
Justine Gauthier <gauthier.justine25000@gmail.com>
2025-03-05 16:36:56 +0100

    mise en place de cookies pour assurer de conserver le panier au rafraichissement pendant 15 minutes

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,13 +1,17 @@
 import { createContext, useContext, useReducer } from "react";
+import Cookies from "js-cookie";
 
 const CartContext = createContext();
 
 const cartReducer = (state, action) => {
   const { type, payload } = action;
-  var existingItem = false;
+  let existingItem = false;
 
-  if (payload)
+  if (payload) {
     existingItem = state.cart.find((item) => item._id === payload._id);
+  }
+
+  const timeBeforeExpiration = new Date(new Date().getTime() + 15 * 60 * 1000);
 
   switch (type) {
     case "add_item":
@@ -18,22 +22,30 @@ const cartReducer = (state, action) => {
               ? { ...item, quantity: item.quantity + 1 }
               : item
           );
-          return {
+          const newState = {
             ...state,
             cart: updatedCart,
             totalItems: state.totalItems + 1,
             totalPrice: state.totalPrice + Number(payload.price),
           };
+          Cookies.set("cart", JSON.stringify(newState.cart), {
+            expires: timeBeforeExpiration,
+          });
+          return newState;
         }
       } else {
-        return {
+        const newState = {
           ...state,
           cart: [...state.cart, { ...payload, quantity: 1 }],
           totalItems: state.totalItems + 1,
           totalPrice: state.totalPrice + Number(payload.price),
         };
+        Cookies.set("cart", JSON.stringify(newState.cart), {
+          expires: timeBeforeExpiration,
+        });
+        return newState;
       }
-      return state;
+      break;
 
     case "remove_item":
       if (existingItem) {
@@ -43,27 +55,38 @@ const cartReducer = (state, action) => {
               ? { ...item, quantity: item.quantity - 1 }
               : item
           );
-          return {
+          const newState = {
             ...state,
             cart: updatedCart,
             totalItems: state.totalItems - 1,
             totalPrice: state.totalPrice - Number(payload.price),
           };
+          Cookies.set("cart", JSON.stringify(newState.cart), {
+            expires: timeBeforeExpiration,
+          });
+          return newState;
         } else {
           const updatedCart = state.cart.filter(
             (item) => item._id !== payload._id
           );
-          return {
+          const newState = {
             ...state,
             cart: updatedCart,
             totalItems: state.totalItems - 1,
             totalPrice: state.totalPrice - Number(payload.price),
           };
+          Cookies.set("cart", JSON.stringify(newState.cart), {
+            expires: timeBeforeExpiration,
+          });
+          return newState;
         }
       }
-      return state;
+      break;
 
     case "clear_cart":
+      Cookies.set("cart", JSON.stringify([]), {
+        expires: timeBeforeExpiration,
+      });
       return {
         ...state,
         cart: [],
@@ -76,14 +99,22 @@ const cartReducer = (state, action) => {
   }
 };
 
-const initialState = {
-  cart: [],
-  totalItems: 0,
-  totalPrice: 0,
+const initialState = () => {
+  const cartFromCookies = Cookies.get("cart");
+  if (cartFromCookies) {
+    const cart = JSON.parse(cartFromCookies);
+    const totalItems = cart.reduce((total, item) => total + item.quantity, 0);
+    const totalPrice = cart.reduce(
+      (total, item) => total + item.quantity * item.price,
+      0
+    );
+    return { cart, totalItems, totalPrice };
+  }
+  return { cart: [], totalItems: 0, totalPrice: 0 };
 };
 
 const CartProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(cartReducer, initialState);
+  const [state, dispatch] = useReducer(cartReducer, initialState());
 
   const value = {
     cart: state.cart,
